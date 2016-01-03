57e3e66f929b0096a0261d009d2724849b42f41d
Caleb Spare <cespare@gmail.com>
2016-01-03 00:45:15 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# gogo
+
+gogogo is gogoprotobuf vanity binary that is like gogofast, but generates more idiomatic Go names for fields.
+For example, the proto field `customer_id` becomes `CustomerID`, not `CustomerId`.
+
+TODO: Also fix field and message names.
---
+++ cmd/protoc-gen-gogogo/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gogo/protobuf/gogoproto"
+	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
+	"github.com/gogo/protobuf/vanity"
+	"github.com/gogo/protobuf/vanity/command"
+)
+
+func main() {
+	req := command.Read()
+	files := req.GetProtoFile()
+	files = vanity.FilterFiles(files, vanity.NotInPackageGoogleProtobuf)
+
+	vanity.ForEachFile(files, vanity.TurnOnMarshalerAll)
+	vanity.ForEachFile(files, vanity.TurnOnSizerAll)
+	vanity.ForEachFile(files, vanity.TurnOnUnmarshalerAll)
+
+	vanity.ForEachFieldInFiles(files, FixFieldNames)
+
+	resp := command.Generate(req)
+	command.Write(resp)
+}
+
+func FixFieldNames(field *descriptor.FieldDescriptorProto) {
+	if FieldHasStringExtension(field, gogoproto.E_Customname) {
+		return
+	}
+	name, ok := SnakeToCamel(field.GetName())
+	if !ok {
+		return
+	}
+	fmt.Fprintln(os.Stderr, name)
+	if field.Options == nil {
+		field.Options = &descriptor.FieldOptions{}
+	}
+	if err := proto.SetExtension(field.Options, gogoproto.E_Customname, &name); err != nil {
+		panic(err)
+	}
+}
+
+func FieldHasStringExtension(field *descriptor.FieldDescriptorProto, extension *proto.ExtensionDesc) bool {
+	if field.Options == nil {
+		return false
+	}
+	value, err := proto.GetExtension(field.Options, extension)
+	if err != nil {
+		return false
+	}
+	if value == nil {
+		return false
+	}
+	if value.(*string) == nil {
+		return false
+	}
+	return true
+}
+
+func SnakeToCamel(snake string) (camel string, changed bool) {
+	parts := strings.Split(snake, "_")
+	for _, part := range parts {
+		if commonInitialisms[part] {
+			changed = true
+			camel += strings.ToTitle(part)
+		} else {
+			camel += strings.Title(part)
+		}
+	}
+	return camel, changed
+}
+
+// Taken from golint (with some additions).
+var commonInitialisms = map[string]bool{
+	"api":   true,
+	"ascii": true,
+	"cpu":   true,
+	"css":   true,
+	"dns":   true,
+	"eof":   true,
+	"guid":  true,
+	"html":  true,
+	"http":  true,
+	"https": true,
+	"id":    true,
+	"ip":    true,
+	"json":  true,
+	"lhs":   true,
+	"os":    true,
+	"qps":   true,
+	"ram":   true,
+	"rhs":   true,
+	"rpc":   true,
+	"sla":   true,
+	"smtp":  true,
+	"sql":   true,
+	"ssh":   true,
+	"tcp":   true,
+	"tls":   true,
+	"ttl":   true,
+	"udp":   true,
+	"ui":    true,
+	"uid":   true,
+	"uri":   true,
+	"url":   true,
+	"utf8":  true,
+	"uuid":  true,
+	"vm":    true,
+	"xml":   true,
+	"xsrf":  true,
+	"xss":   true,
+}
